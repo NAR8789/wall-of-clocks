d6da367b2ce6acfa16103458fd64446a25bf5447
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2020-09-19 21:47:17 -0700

    clear out the cruft

--- pages/index.js
+++ pages/index.js
@@ -6,24 +6,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Wall of Clocks</title>
       </Head>
 
       <main className={styles.main}>
         <Clock/>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
